86fbdcaf57e6a54a5bbeb13e35f093d24439aa19
Math-SA <matheus.s.assis@gmail.com>
2022-09-28 18:25:48 -0300

    formatted header

--- Components/Header.js
+++ Components/Header.js
@@ -6,9 +6,8 @@ function Header() {
     <div className="header">
       <h1>Cryptonis Reaction</h1>
       <p>
-        {' '}
         a react based frontend for
-        <a href="https://github.com/Math-SA/Cryptonis"> Cryptonis </a>
+        <a href="https://github.com/Math-SA/Cryptonis"> Cryptonis</a>
       </p>
     </div>
   );
