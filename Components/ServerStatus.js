1dce06269cf52a3ba5206bc5d763660812555113
Math-SA <matheus.s.assis@gmail.com>
2022-09-29 11:18:48 -0300

    Add server ping (disabled)

--- Components/ServerStatus.js
+++ Components/ServerStatus.js
@@ -1,9 +1,30 @@
 import * as React from 'react';
 import { useState } from 'react';
 import './ServerStatus.css';
+import Axios from 'axios';
 
-function ServerStatus() {
-  const [serverStatusClass, setServerStatusClass] = useState('status offline');
-  return <div className={serverStatusClass}></div>;
+function ServerStatus(props) {
+  const serverPingUrl = 'https://devplay-cryptonis.onrender.com/ping';
+  const pingAllowed = false;
+  const pingInterval = 3000;
+  const pingServer = () => {
+    Axios.get(serverPingUrl).then((res) => {
+      props.repaint(res.status == 200);
+    });
+  };
+
+  const setServerStatusClass = () => {
+    if (props.online) {
+      return 'status online';
+    } else {
+      return 'status offline';
+    }
+  };
+
+  if (pingAllowed) {
+    setTimeout(pingServer, pingInterval);
+  }
+
+  return <div className={setServerStatusClass()}></div>;
 }
 export default ServerStatus;
